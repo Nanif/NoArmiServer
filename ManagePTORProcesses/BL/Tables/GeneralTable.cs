97236a0d358fabd0612afe8994888024cc10e87a
Nanif <40644835+Nanif@users.noreply.github.com>
2019-05-05 00:52:59 +0300

    first commit

---
+++ ManagePTORProcesses/BL/Tables/GeneralTable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace BL.Tables
+{
+    public class GeneralTable
+    {
+        private static DAL.ConnectToAccess connectAccess;
+
+        private DataTable table;
+
+
+        // static ctor
+        static GeneralTable()
+        {
+            connectAccess = new DAL.ConnectToAccess();
+        }
+
+
+        public GeneralTable(string tableName)
+        {
+            if (table != null)
+                table = connectAccess.GetTable(tableName);
+
+
+        }
+    }
+}
---
+++ ManagePTORProcesses/DAL/ConnectToAccess.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DAL
+{
+    public class ConnectToAccess
+    {
+        private DataSet dataset;
+        private OleDbConnection connection;
+
+        Properties.Settings settings = new Properties.Settings();
+
+        public ConnectToAccess()
+        {
+            dataset = new DataSet();
+            if (File.Exists(settings.DBPath))
+            {
+                connection = new OleDbConnection(settings.DBPath);
+            }
+            else
+                throw new Exception("מסד הנתונים לא נמצא בנתיב שצוין");
+        }
+
+        private void AddTable(string tableName)
+        {
+            OleDbDataAdapter adapter = new OleDbDataAdapter("select * from " + tableName, connection);
+            adapter.Fill(dataset, tableName);
+
+        }
+
+        public DataTable GetTable(string tableName)
+        {
+            if (!dataset.Tables.Contains(tableName))
+                AddTable(tableName);
+            return dataset.Tables[tableName];
+        }
+
+
+        public void SaveTable(string tableName)
+        {
+            OleDbDataAdapter adapter = new OleDbDataAdapter("select * from " + tableName, connection);
+            OleDbCommandBuilder builder = new OleDbCommandBuilder(adapter);
+            adapter.DeleteCommand = builder.GetDeleteCommand();
+            adapter.UpdateCommand = builder.GetUpdateCommand();
+            adapter.InsertCommand = builder.GetInsertCommand();
+            adapter.Update(dataset, tableName);
+        }
+
+        public void SaveAllTables()
+        {
+            foreach (DataTable item in dataset.Tables)
+            {
+                UpdateTable(item.TableName);
+            }
+        }
+
+    }
+}
